99d599d41ef2439105dce5f67d4dc81ec1ca080a
Matheus snoopy Alves <118631272+MatheusAlves95@users.noreply.github.com>
2023-02-25 19:12:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,79 @@
+'''
+Autor: Matheus Snoopy
+Data: 25/02/23
+Local: Fortalza
+'''
+# Meu primeiro Código
+print('olá')
+print('Tudo Bem?')
+print('Que dia da semana é hoje?')
+
+# Meu segundo código
+print('Sabadou')
+
+
+#testando variavel
+X = 'Sabadu'
+print(X)
+
+#criando variaveis e modificando dados
+
+x = str(3)
+y = int(4)
+z = float(5)
+
+
+print(x+x)
+print(y+y)
+print(z+z)
+
+# criando frase, adicionando variaveis e print no final.
+
+# O Matheus tem 28 anos de idade e mora na cidade de Fortaleza.;
+
+nome = 'Matheus'
+idade = 28
+idade = str (idade)
+cidade = 'Fortaleza'
+
+print(' O ' + nome + ' tem ' + idade +  ' anos de idade e mora em ' + cidade + '.')
+
+
+# criando sozinho texto com variaiveis
+
+# O Matheus tem 28 anos de idade, estuda barbearia e programação, tem 1,85 metros de altura, e mora na cidade de Fortaleza.
+
+
+nome = 'Matheus'
+idade = 28
+idade = str (idade)
+curso1 = 'barbearia'
+curso2 = 'programação'
+medida = 1.85
+medida = str(medida)
+
+print(' O ' + nome + ' tem ' + idade + ' de idade ' +  ' estuda ' + curso1 + ' e '+ curso2 + ' tem ' + medida + ' de altura ' + ' e mora na cidade de ' + cidade + ".")
+
+#adicionando INPUT para interagir com clientes e usuários. usando a mesma frase acima.
+
+nome = input('digite seu nome:')
+idade = input('digite sua idade:')
+idade = str(idade)
+curso1 = input ('digite seu primeiro curso:')
+curso2 = input ('digite seu segundo curso:')
+medida = input('digite sua altura:')
+medida = str (medida)
+cidade = input('Digite sua cidade:')
+cidade = str(cidade)
+
+print('O ' + nome + ' tem ' + idade + ' de idade estuda ' + curso1 + ' e ' + curso2 + ' tem ' + medida + ' de altura e mora na cidade de ' + cidade +'.')
+
+# fim do primeiro dia
+print('primeiro dia de aula concluido estou apaixonado pelo python!')
+
+
+
+
+
+
+
